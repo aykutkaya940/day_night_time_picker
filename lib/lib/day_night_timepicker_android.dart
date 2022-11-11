0b0848d1d9ef58f87bf7cec8129feae7c25b4d29
Aykut Kaya <aykutkaya940@gmail.com>
2022-11-11 16:13:32 +0300

    Update day_night_timepicker_android.dart

--- lib/lib/day_night_timepicker_android.dart
+++ lib/lib/day_night_timepicker_android.dart
@@ -99,7 +99,7 @@ class DayNightTimePickerAndroidState extends State<DayNightTimePickerAndroid> {
                             children: <Widget>[
                               SizedBox(
                                 width: 90,
-                                height: 100,
+                                height: 150,
                                 child: TextField(
                                   maxLength: 2,
                                   focusNode: hourFocus,
@@ -155,7 +155,7 @@ class DayNightTimePickerAndroidState extends State<DayNightTimePickerAndroid> {
                               ),
                               SizedBox(
                                   width: 30,
-                                  height: 120,
+                                  height: 150,
                                   child: IgnorePointer(
                                     ignoring: true,
                                     child: TextField(
@@ -189,7 +189,7 @@ class DayNightTimePickerAndroidState extends State<DayNightTimePickerAndroid> {
 
                               SizedBox(
                                 width: 90,
-                                height: 120,
+                                height: 150,
                                 child: TextField(
                                   maxLength: 2,
                                   keyboardType: TextInputType.number,
